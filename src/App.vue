05c86bd8ba511995aee9d58d450fa9e2e738cdea
Aung-Thu-Heain <mrfoethu@gmail.com>
2025-02-18 00:32:15 +0630

    initia

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script >
+export default{
+  data(){
+    return{
+      name:"Ei hlaing",
+    }
+  }
+}
+</script>
+
+<template>
+ <h1>{{ name }}</h1>
+</template>
+
+<style scoped>
+h1{
+  color: red;
+}
+</style>
